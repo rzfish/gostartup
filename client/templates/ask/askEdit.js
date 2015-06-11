204c4e263a13a245fff6d40c4f8944405cd8ccf1
rzfish <rzfish@126.com>
2015-06-11 09:29:52 +0800

    add ask edit

--- client/templates/ask/askEdit.js
+++ client/templates/ask/askEdit.js
@@ -4,11 +4,24 @@ Template.askEdit.events({
         e.preventDefault();
         t = $(e.target);
         var ask = {
+            //author: Meteor.user(),
             title: t.find('[name=title]').val(),
             tag: t.find('[name=tag]').val(),
             detail: t.find('[name=detail]').val(),
         };
-        Asks.insert(ask);
+
+        var id = t.find('[name=id]').val();
+        if(id == "") {
+            Asks.insert(ask);
+        } else {
+            ask._id = id;
+            Asks.update({_id: id}, ask);
+        }
         Router.go('askList');
     },
-})
+});
+
+Template.askEdit.onRendered(function() {
+        this.$('#tag').val(this.data.tag);
+    }
+);
