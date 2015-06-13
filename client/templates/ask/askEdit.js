217b654734d8d5571fc5bfe08293449733c3b52d
rzfish <rzfish@126.com>
2015-06-13 16:28:58 +0800

    update the ask edit to contain more meaningful info

--- client/templates/ask/askEdit.js
+++ client/templates/ask/askEdit.js
@@ -3,19 +3,25 @@ Template.askEdit.events({
     'submit form' : function(e) {
         e.preventDefault();
         t = $(e.target);
+
         var ask = {
-            //author: Meteor.user(),
             title: t.find('[name=title]').val(),
             tag: t.find('[name=tag]').val(),
             detail: t.find('[name=detail]').val(),
         };
 
+        alert(gostart.printObject(Meteor.user()));
+        return;
         var id = t.find('[name=id]').val();
-        if(id == "") {
+        if(id == "") {  //new ask
+            author: Meteor.user().username;
+            ask.created = gostart.getTimeStr();
+            ask.updated = ask.created;
+            ask.reCnt = 0;
+            ask.up = 0;
             Asks.insert(ask);
         } else {
-            ask._id = id;
-            Asks.update({_id: id}, ask);
+            Asks.update({_id: id}, {$set: ask});
         }
         Router.go('askList');
     },
