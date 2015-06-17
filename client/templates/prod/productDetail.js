294230ee240b56739d8b2f7eb50c255b52e694ae
rzfish <rzfish@126.com>
2015-06-18 07:45:29 +0800

    local remove product

--- client/templates/prod/productDetail.js
+++ client/templates/prod/productDetail.js
@@ -5,6 +5,14 @@ Template.prodDetail.events({
     },
     'click #btn_reply': function(e) {
         Router.go('/product/reply/' + this._id)
+    },
+    
+    'click #btn_del': function(e) {
+        var r = confirm("你确定要删除该产品？")
+        if(r == true) {
+            Products.remove({_id: this._id});
+            Router.go('prodList');
+        }
     }
 
 })
