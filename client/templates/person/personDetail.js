a3ddb5a9afd8a4ad7f40e633b2c59cd998672151
rzfish <rzfish@126.com>
2015-06-19 23:39:09 +0800

    only root could remove user

--- client/templates/person/personDetail.js
+++ client/templates/person/personDetail.js
@@ -2,12 +2,25 @@
 Template.personDetail.events({
     'click #btn_update': function(e) {
         Router.go('/person/update/' + this._id)
-    }
+    },
+    'click #btn_del' : function(e) {
+        e.preventDefault();
+        var r = confirm("你确定要删除这个用户吗？");
+        if (r == true){
+            Persons.remove({_id: this._id});
+            Router.go('personList');
+        }
+    },
+
 });
 
 Template.personDetail.helpers({
     owns: function() {
         return (this._id == Meteor.userId()) || 
             (Meteor.user().username == 'root');  // tmp solution for admin
+    },
+    isRoot: function() {
+        return (Meteor.user().username == 'root');  // tmp solution for admin
     }
+
 })
